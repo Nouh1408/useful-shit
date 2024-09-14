346bed9c4a920c207c9971a37ac146e425b8dfc3
Nouh1408 <ahmedinouh@gmail.com>
2024-09-14 06:33:46 +0300

    now hovering mouse over and mouse out

--- Keytracker/script.js
+++ Keytracker/script.js
@@ -1,3 +1,11 @@
 $("input").keypress(function(event){
-    console.log(event.key)
+    // console.log(event.key)
+    $("h1").text(event.key)
+})
+
+$("h1").on("mouseover", function(){
+    $(this).css("color","purple")
+})
+$("h1").on("mouseout", function(){
+    $(this).css("color","black")
 })
